693df8330ab74c9864c10aa37d70d9bd32d8c846
UnderShot90 <124603502+UnderShot90@users.noreply.github.com>
2025-05-08 18:03:55 -0400

    Add files via upload

---
+++ SnakeBlock.java
@@ -0,0 +1,34 @@
+package SnakeGame;
+
+public class SnakeBlock {
+	private int xaxis;
+	private int yaxis;
+	
+	public static final int board_height = 20;
+	public static final int board_width = 20;
+	
+	public SnakeBlock( int xaxis, int yaxis) {// get pos of snake block
+		this.xaxis = xaxis;
+		this.yaxis = yaxis;
+	}
+	public boolean collision(SnakeBlock r2) {
+		// this returns if x and y are the same as r2
+		
+		return this.xaxis == r2.getXaxis() && this.yaxis == r2.getYaxis();
+	}
+	
+	public void setXaxis(int increment) {
+		this.xaxis= this.xaxis+increment; 
+	}
+	public void setYaxis(int increment) {
+		this.yaxis= this.yaxis+increment; 
+	}
+	public int getXaxis() {
+		return this.xaxis;
+	}
+	public int getYaxis() {
+		return this.yaxis;
+	}
+
+	
+}
